daeec05ab817cdc849493a3e6c7d3ff5379d7123
Sandro <sandro@alphonse.xyz>
2020-11-13 17:01:34 +0100

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,12 +1,20 @@
-<h1>QCM</h1>
+<!doctype html>
+<title>Labo Excelia Digital School - PHP</title>
+<meta charset="UTF-8" />
+<html lang=”fr-FR”>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
 
-<?php
-  echo 'Bonjour l\'école est ouverte à 8h00';
-?>
-
-<h2>Tableau :</h2>
-<?php
-  
-for ($i=0; $i<3; $i++) { echo $i; }
-
-?>
+<article>
+    <h1><a href="https://www.alphonse.xyz/">Consultez mon exercice concernant un CV modifié en PHP</a></h1>
+    <div>
+      
+  <p><a href="https://www.alphonse.xyz/" target="_blank">Sandro</a></p>
+    </div>
+</article>
